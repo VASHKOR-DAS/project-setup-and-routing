e0e5eb40be7ba539ad849244b594092bc77829de
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-09-23 23:33:49 +0600

    dinamically set route

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
+
+    // useParams() diye dynamically route show korano hoy
+    const {friendId} = useParams();
     return (
         <div>
-            <h3>FriendDetails</h3>
+            <h3>Friend details components : {friendId}</h3>
         </div>
     );
 };
