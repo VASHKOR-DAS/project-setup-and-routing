46a82d99aeb2127ca2728adf1100ab99ac962f27
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-09-23 15:45:50 +0600

    -redirects set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Home from './components/Home/Home';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import NoMatch from './components/NoMatch/NoMatch';
+
 function App() {
 
   return (
     <div>
-      <Home/>
+      <Router>
+        <Routes>
+
+          <Route exact path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+
+          <Route path='*' element={<NoMatch />}></Route>
+
+
+
+
+        </Routes>
+      </Router>
     </div>
   );
 }
